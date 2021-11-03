ff1df821d7197107c99fe1f1b0d08724b11a1670
kobby <asieduk1528@gmail.com>
2021-11-03 13:52:29 +0000

    Login page

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from './Screen/Homepage';
+import Login from './Screen/Login';
+import Cart from './Screen/cart';
+import { FontAwesome } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Homepage') {
+              iconName = focused
+                ? 'home'
+                : 'home';
+            } else if (route.name === 'Login') {
+              iconName = focused ? 'user' : 'user';
+            } if (route.name === 'Cart') {
+              iconName = focused
+                ? 'shopping-cart'
+                : 'shopping-cart';
+            }
+
+            // You can return any component that you like here!
+            return <FontAwesome name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
+        <Tab.Screen name="Homepage" component={Home} />
+        <Tab.Screen name="Login" component={Login} />
+        <Tab.Screen name="Cart" component={Cart} />
+        
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
